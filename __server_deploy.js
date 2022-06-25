b60cdc616f046c8ea76f085255c94c937642b122
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-06-25 16:32:07 +0600

    we are create a heroku deploy site

---
+++ __server_deploy.js
@@ -0,0 +1,29 @@
+/**
+ * ---------------------------
+ * ONE TIME for my computer
+ * ---------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ---------------------------
+ * For each project on time
+ * ---------------------------
+ * 1. heroku create
+ * 2. make sure you: git add ., git commit -m "", git push
+ * 3. git push heroku main
+ * 4. Go To Heroku Dashboard > Current Project > Settings > Reveal Config vars
+ * 5. copy paste config vars from my .env file
+ * 6. Make sure you have whitelisted all ip address to access mongodb
+ * --------------------------------
+ * Update Server with new changes
+ * --------------------------------
+ * 1. Make Changes (any line codes)
+ * 2. make sure you: git add ., git commit -m "", git push
+ * 3. git push heroku main
+ *
+ *
+ * ------------------------------
+ * Connect Server with Client
+ * ------------------------------
+ */
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,22 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+  });
+  next();
+}
 
 // password : dZuMeN2QIm8um3K5
 
@@ -22,7 +39,18 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
 
+    // AUTH access token secret
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // service api
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -52,6 +80,27 @@ async function run() {
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
+
+    // Order Collection API
+    app.get("/order", verifyJWT, async (req, res) => {
+      const decodedEmail = req.destroy.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const order = await cursor.toArray();
+        res.send(order);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    // Order Collection API or Order user info
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
   } finally {
   }
 }
@@ -66,6 +115,11 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running Genius Server");
+});
+
+// test update server site to heroku (optional)
+app.get("/hero", (req, res) => {
+  res.send("test update server site to heroku (optional)");
 });
 
 app.listen(port, () => {
